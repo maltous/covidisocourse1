1d9a556e1add3efc67ad2ae7cbca1fd0c8b8854c
nsddksmc <40565225+nsddksmc@users.noreply.github.com>
2020-04-27 01:05:50 +0300

    covidisocourse1

---
+++ story_content/user.js
@@ -0,0 +1,47 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "650Ct5aC8ad":
+        Script1();
+        break;
+      case "689jwmxu2N5":
+        Script2();
+        break;
+      case "5ykYRq7c739":
+        Script3();
+        break;
+      case "6BpacLr8mfr":
+        Script4();
+        break;
+      case "5jlJcdDYsW4":
+        Script5();
+        break;
+  }
+}
+
+function Script1()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script2()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script3()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script4()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script5()
+{
+  window.print();
+}
+
